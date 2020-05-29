f03e6f3f827873091785ed565da09aaa8ccbc12e
revolution <revolution@debian.localdomain>
2020-05-29 19:40:29 -0400

    database_updater.py

---
+++ database_updater.py
@@ -0,0 +1,53 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import urllib.parse
+import secrets
+import json
+import os
+import re
+
+cred = credentials.Certificate("sheep16-spacesheep-firebase-adminsdk-qjr6i-1839d2596a.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+arr = os.listdir("missions/")
+type = []
+
+for a in arr:
+	type = a.split('.')
+	if(type[1] == "md"):
+		file = open("missions/" + a, "+r")
+		content = file.read()
+		match = re.search("---(.|\n)*?---", content)
+		target = match.group()
+		
+		target = re.sub("---", "{", target, 1)
+		target = re.sub("---", "}", target, 1)
+		target = re.sub("\n  - ", ", ", target)
+		target = re.sub(":, ", "\":[", target)
+		target = re.sub("\n    ", ", ", target)
+		target = re.sub(r"(\[.*)", r"\1]", target)
+		target = re.sub("\n", "\",\n\"", target)
+		target = re.sub(", ", "\", \"", target)
+		target = re.sub(": ", "\":\"", target)
+		target = re.sub("\[", "[\"", target)
+		target = re.sub("\]\"", "\"]", target)
+		target = re.sub("\{\",", "{", target)
+		target = re.sub(",\n\"\}", "\n}", target)
+		target = re.sub("\"label", "{\"label", target)
+		target = re.sub(r"(\"href\":\".*\")", r"\1}", target)
+		target = re.sub(", \{", "}, {", target)
+		target = re.sub(",\n", ",\n\"slug\":\"" + type[0] + "\",\n", target, 1)
+		target = re.sub(",\n", ",\n\"type\":\"mission\",\n", target, 1) 
+		
+		d = json.loads(target)
+		print(d)	
+		
+		id = secrets.token_urlsafe(20)
+
+		doc_ref = db.collection('example').document(id)
+		doc_ref.set(d)
+	
+print(arr)
